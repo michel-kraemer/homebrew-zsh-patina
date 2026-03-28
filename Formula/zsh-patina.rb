2560d0ccd494949cf65297a3efd61f00233b2c81
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-03-28 08:36:43 +0100

    Bump up version number to 1.2.0

--- Formula/zsh-patina.rb
+++ Formula/zsh-patina.rb
@@ -3,11 +3,11 @@ class ZshPatina < Formula
   homepage "https://github.com/michel-kraemer/zsh-patina"
 
   if Hardware::CPU.arm?
-    url "https://github.com/michel-kraemer/zsh-patina/releases/download/1.1.0/zsh-patina-v1.1.0-aarch64-apple-darwin.tar.gz"
-    sha256 "b781b6462f6bc5ef4d2d6f1ddeb0e5e5e2e7d6b93293abcb0d3a5e6c210bfd63"
+    url "https://github.com/michel-kraemer/zsh-patina/releases/download/1.2.0/zsh-patina-v1.2.0-aarch64-apple-darwin.tar.gz"
+    sha256 "d9c56208f886dd35bd98a334c7cdb0333552929efd11e6a2682d980b00f3ba66"
   else
-    url "https://github.com/michel-kraemer/zsh-patina/releases/download/1.1.0/zsh-patina-v1.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "aa07b78f677f434947f1adb6ca52f0dc5f4a021c6f4c54e73ac8c5397dfc08f4"
+    url "https://github.com/michel-kraemer/zsh-patina/releases/download/1.2.0/zsh-patina-v1.2.0-x86_64-apple-darwin.tar.gz"
+    sha256 "2c2a8e7286a2db6fb61bb5b9278a2e6a3643e538807d047a639a5a13e3ab64a7"
   end
 
   def install
